9e3e9cbde06bfe473d05ff74b2fc5e3af6bf690a
Michael Morrison <morri909@umn.edu>
2021-03-11 09:28:00 -0600

    Update thymeleaf to use reactive

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -18,7 +18,7 @@ public class IndexController {
 
 	@RequestMapping({"", "/", "/index"})
 	public String getIndexPage(Model model){
-		model.addAttribute("recipes", recipeService.getRecipes().collectList());
+		model.addAttribute("recipes", recipeService.getRecipes());
 		return "index";
 	}
 }
